410880b70c0d580334e95eab7da6024b7f204091
vamastah <szymorawski@gmail.com>
2024-11-01 16:12:19 +0100

    first working proof of concept

--- avr/main.c
+++ avr/main.c
@@ -7,68 +7,61 @@
 
 #include "fsm.h"
 
-#define CYCLE_SIZE	(16)
-#define CYCLE_MASK	(0x0f)
+union Num
+{
+	uint32_t v32;
+	struct
+	{
+		uint16_t lsp;
+		uint16_t msp;
+	};
+};
+
+void serial_init(void);
+
+#define CYCLE_SIZE	(128)
+#define CYCLE_MASK	(0x7f)
 
 // start, mod 8 prescaler (2 MHz)
 #define timer_start() do { TCCR1B = _BV(CS11); } while (0);
 #define timer_stop() do { TCCR1B = 0; } while (0);
-// get time in us
-#define timer_get() ((((uint32_t)count) << 15) | (((uint32_t)TCNT1) >> 1))
-#define timer_clear() do { TCNT1 = 0; count = 0; } while (0);
-
 #define cycle_forward(x) do { x = (x + 1) & CYCLE_MASK; } while (0);
 
 static volatile uint16_t count = 0;
 
-static volatile uint32_t ts[CYCLE_SIZE];
-static volatile uint8_t val[CYCLE_SIZE];
+static volatile union Num ts[CYCLE_SIZE];
 static volatile uint8_t cbegin = 0;
 static volatile uint8_t cend = 0;
-
-static int sendc(char c, FILE *f);
-static FILE defstream = FDEV_SETUP_STREAM(sendc, NULL, _FDEV_SETUP_RW);
-
-void serial_init(void)
-{
-#define BAUD 19200
-#include <util/setbaud.h>
-	UBRRH = UBRRH_VALUE;
-	UBRRL = UBRRL_VALUE;
-	UCSRB = (1 << TXEN) | (1 << RXEN);
-#if USE_2X
-	UCSRA |= (1 << U2X);
-#else
-	UCSRA &= ~(1 << U2X);
-#endif
-#undef BAUD
-	stdout = stdin = &defstream;
-}
-
-static int sendc(char c, FILE *f)
-{
-	// wait for empty transmit buffer
-	while ( !( UCSRA & (1 << UDRE) ) );
-	UDR = c;
-	return 0;
-}
 
 ISR(TIMER1_OVF_vect)
 {
 	++count;
 }
 
-// higher priority than timer1
 ISR(INT0_vect)
 {
-	val[cend] = PIND & PIND2;
-	ts[cend] = timer_get();
+	ts[cend].lsp = TCNT1;
+	TCNT1 = 0;
+	ts[cend].msp = count;
+	count = 0;
+	ts[cend].lsp &= 0xfffe;
+	cycle_forward(cend);
+}
+
+ISR(INT1_vect)
+{
+	ts[cend].lsp = TCNT1;
+	TCNT1 = 0;
+	ts[cend].msp = count;
+	count = 0;
+	ts[cend].lsp |= 0x0001;
 	cycle_forward(cend);
 }
 
 void flat_callback(uint8_t flatno)
 {
-	PORTB = ~flatno;
+	//PORTB = ~flatno;
+	printf("m:publish(topic..\"flat\", %d, 0, 0)\r\n", flatno);
 }
 
 void main(void)
@@ -84,26 +77,25 @@ void main(void)
 	TCCR1A = 0x00;
 	timer_start();
 
-	// default INT0 config as tri-z input is ok
-	// configure external interrupt as dual edge
-	MCUCR = _BV(ISC00);
+	// default INT0 and INT1 config as tri-z input is NOT ok
+	PORTD = _BV(PIND2) | _BV(PIND3);
+	// configure INT0 as falling edge, INT1 as raising edge
+	MCUCR = _BV(ISC01) | _BV(ISC11) | _BV(ISC10);
 	// enable external interrupt
-	GICR = _BV(INT0);
+	GICR = _BV(INT0) | _BV(INT1);
 
 	fsm_reset();
 	fsm_set_cb(flat_callback);
 
-	uint32_t last_ts = 0;
-
 	DDRB = 0xff;
 
-	printf("Start!\r\n");
+	printf("Proel Sniffer MCU\r\n");
 	while (1)
 	{
 		if (cbegin != cend)
 		{
-			fsm_push_event(val[cbegin], ts[cbegin] - last_ts);
-			last_ts = ts[cbegin];
+			fsm_push_event(ts[cbegin].lsp & 1, ts[cbegin].v32 >> 1);
+			//printf("%d %ld\r\n", ts[cbegin].lsp & 1, ts[cbegin].v32 >> 1);
 			cycle_forward(cbegin);
 		}
 	}
---
+++ avr/serial.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include <stdio.h>
+
+static int sendc(char c, FILE *f);
+static FILE defstream = FDEV_SETUP_STREAM(sendc, NULL, _FDEV_SETUP_RW);
+
+void serial_init(void)
+{
+#define BAUD 19200
+#include <util/setbaud.h>
+	UBRRH = UBRRH_VALUE;
+	UBRRL = UBRRL_VALUE;
+	UCSRB = (1 << TXEN) | (1 << RXEN);
+#if USE_2X
+	UCSRA |= (1 << U2X);
+#else
+	UCSRA &= ~(1 << U2X);
+#endif
+#undef BAUD
+	
+	stdout = stdin = &defstream;
+}
+
+static int sendc(char c, FILE *f)
+{
+	// wait for empty transmit buffer
+	while ( !( UCSRA & _BV(UDRE) ) );
+	UDR = c;
+	return 0;
+}
